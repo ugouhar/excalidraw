983f0c7ef3923dbd784bf2a000b68b7f12f27f9f
Ushran Gouhar <ugouhar@atlassian.com>
2025-07-27 22:15:52 +0530

    One rectangle with direction from top-left to bottom-right

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let prevWidth = 0;
 let prevHeight = 0;
+let isDrawingEnabled = false;
+let isMousePositionForStartingCoordinates = true;
+let x_coord_start = 0;
+let y_coord_start = 0;
+
 function drawRectangle(event) {
-  const x_coord_start = 10;
-  const y_coord_start = 10;
+  if (!isDrawingEnabled) return;
+  if (isMousePositionForStartingCoordinates) {
+    x_coord_start = event.clientX;
+    y_coord_start = event.clientY;
+    isMousePositionForStartingCoordinates = false;
+  }
+
   ctx.clearRect(x_coord_start, y_coord_start, prevWidth, prevHeight);
   ctx.clearRect(
     x_coord_start - 1,
@@ -14,12 +24,16 @@ function drawRectangle(event) {
   );
 
   const rectangle = new Path2D();
-  const width = event.clientX;
-  const height = event.clientY;
+  const width = event.clientX - x_coord_start;
+  const height = event.clientY - y_coord_start;
   rectangle.rect(x_coord_start, y_coord_start, width, height);
   ctx.stroke(rectangle);
   prevWidth = width;
   prevHeight = height;
 }
-
-canvas.addEventListener("click", drawRectangle);
+canvas.addEventListener("mousedown", () => (isDrawingEnabled = true));
+canvas.addEventListener("mouseup", () => {
+  isDrawingEnabled = false;
+  isMousePositionForStartingCoordinates = true;
+});
+canvas.addEventListener("mousemove", drawRectangle);
