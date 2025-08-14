536cf2181955c0620aa4439ff9e18f8ecf844044
ugouhar <ushran987@gmail.com>
2025-08-15 00:23:19 +0530

    Refactor AddShape command

--- commands/add-shape.js
+++ commands/add-shape.js
@@ -2,12 +2,26 @@ export class AddShapeCommand {
   constructor(store, shape) {
     this.store = store;
     this.shape = shape;
+    this.undoStack = [];
+    this.redoStack = [];
   }
   execute() {
     this.store.shapes.push(this.shape);
+    this.undoStack.push(this.shape);
+    this.redoStack = [];
   }
 
   undo() {
+    if (this.undoStack.length === 0) return;
+    const lastShape = this.undoStack.pop();
     this.store.shapes.pop();
+    this.redoStack.push(lastShape);
+  }
+
+  redo() {
+    if (this.redoStack.length === 0) return;
+    const lastShape = this.redoStack.pop();
+    this.store.shapes.push(lastShape);
+    this.undoStack.push(lastShape);
   }
 }
