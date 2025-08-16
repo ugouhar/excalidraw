5bf5ffcee0e7d58e756a896636b68b72ef02a2c7
ugouhar <ushran987@gmail.com>
2025-08-16 12:45:38 +0530

    Add icons and styles

--- constants.js
+++ constants.js
@@ -1,6 +1,9 @@
 export const RECTANGLE = "RECTANGLE";
 export const CIRCLE = "CIRCLE";
+export const ARROW = "ARROW";
 export const LINE = "LINE";
+export const PENCIL = "PENCIL";
+export const TEXT = "TEXT";
 
 export const Rectangle = "Rectangle";
 export const Circle = "Circle";
--- utils/event-listeners.js
+++ utils/event-listeners.js
@@ -1,4 +1,4 @@
-import { CIRCLE, LINE, RECTANGLE } from "../constants.js";
+import { ARROW, CIRCLE, LINE, PENCIL, RECTANGLE, TEXT } from "../constants.js";
 import { store } from "../store.js";
 
 export const registerPageLoadEvent = (computeCanvasPosition) => {
@@ -31,35 +31,67 @@ export const registerCanvasEvents = (
   });
 };
 
-const switchActiveTool = (canvas, toolName) => {
-  store.setIsSelectToolEnabled(toolName === "select-tool");
-  store.setIsMoveToolEnabled(toolName === "move-tool");
-  store.setIsDrawingToolEnabled(toolName === "drawing-tool");
+const switchActiveTool = (canvas, toolType, toolName) => {
+  store.setIsSelectToolEnabled(toolType === "select-tool");
+  store.setIsMoveToolEnabled(toolType === "move-tool");
+  store.setIsDrawingToolEnabled(toolType === "drawing-tool");
 
-  if (toolName === "drawing-tool") canvas.classList.add("canvas-drawing");
+  if (toolType === "drawing-tool") canvas.classList.add("canvas-drawing");
   else canvas.classList.remove("canvas-drawing");
+
+  const allTools = [
+    "select-tool",
+    "rectangle-tool",
+    "circle-tool",
+    "arrow-tool",
+    "line-tool",
+    "pencil-tool",
+    "text-tool",
+  ];
+
+  allTools.forEach((tool) =>
+    document.getElementById(tool).classList.remove("active-tool")
+  );
+  document.getElementById(toolName).classList.add("active-tool");
 };
 
 export const registerToolsEvents = (canvas, undo, redo) => {
   document.getElementById("select-tool").addEventListener("click", () => {
-    switchActiveTool(canvas, "select-tool");
+    switchActiveTool(canvas, "select-tool", "select-tool");
   });
 
   document.getElementById("rectangle-tool").addEventListener("click", () => {
-    switchActiveTool(canvas, "drawing-tool");
+    switchActiveTool(canvas, "drawing-tool", "rectangle-tool");
     store.setShapeSelectedToDraw(RECTANGLE);
   });
 
   document.getElementById("circle-tool").addEventListener("click", () => {
-    switchActiveTool(canvas, "drawing-tool");
+    switchActiveTool(canvas, "drawing-tool", "circle-tool");
     store.setShapeSelectedToDraw(CIRCLE);
   });
 
+  document.getElementById("arrow-tool").addEventListener("click", () => {
+    switchActiveTool(canvas, "drawing-tool", "arrow-tool");
+    store.setShapeSelectedToDraw(ARROW);
+  });
+
   document.getElementById("line-tool").addEventListener("click", () => {
-    switchActiveTool(canvas, "drawing-tool");
+    switchActiveTool(canvas, "drawing-tool", "line-tool");
     store.setShapeSelectedToDraw(LINE);
+  });
+
+  document.getElementById("pencil-tool").addEventListener("click", () => {
+    switchActiveTool(canvas, "drawing-tool", "pencil-tool");
+    store.setShapeSelectedToDraw(PENCIL);
+  });
+
+  document.getElementById("text-tool").addEventListener("click", () => {
+    switchActiveTool(canvas, "drawing-tool", "text-tool");
+    store.setShapeSelectedToDraw(TEXT);
   });
 
   document.getElementById("undo-tool").addEventListener("click", undo);
   document.getElementById("redo-tool").addEventListener("click", redo);
 };
+
+// REFACTOR THIS FILE
