09c4b62835cf45615a0a58af3e3b0b0641e6a16e
ugouhar <ushran987@gmail.com>
2025-08-17 21:01:48 +0530

    Update toolbar web component

--- components/toolbar.js
+++ components/toolbar.js
@@ -4,45 +4,7 @@ template.innerHTML = `
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
     />
-    <style>
-        #toolbar{
-            background-color: white;
-            width: max-content;
-            position: fixed;
-            left: 30vw;
-            top: 15px;
-            display: flex;
-            z-index: 1;
-            gap: 10px;
-            padding: 4px;
-            border-radius: 0.5rem;
-            box-shadow: 0px 0px 0.9px 0px rgba(0, 0, 0, .17), 0px 0px 3px 0px rgba(0, 0, 0, .08), 0px 7px 14px 0px rgba(0, 0, 0, .05);
-            color: rgb(27, 27, 31);
-        }
-        #toolbar i{
-            cursor: pointer;
-            padding: 10px;
-            border: 1px solid transparent;
-            border-radius: 0.5rem;
-        }
-
-        #toolbar i:hover{
-            background-color: #f1f0ff;
-        }
-
-        #toolbar i:active{
-            border: 1px solid #4440bf;
-        }
-
-        #toolbar .active-tool:hover{
-            background-color: #e0dfff;
-        }
-
-        #toolbar .active-tool{
-            background-color: #e0dfff;
-        }
-    </style>
-
+   <link rel="stylesheet" href="./components/toolbar.css" />
     <div id="toolbar">
         <i class="fa-solid fa-arrow-pointer" id="select-tool"></i>
         <i class="fa-regular fa-square" id="rectangle-tool"></i>
