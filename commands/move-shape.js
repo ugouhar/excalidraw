2a759123d9ada282dde77176c403ce5a182d5b3d
ugouhar <ushran987@gmail.com>
2025-08-19 00:06:07 +0530

    Undo redo for line and arrow

--- commands/move-shape.js
+++ commands/move-shape.js
@@ -1,3 +1,5 @@
+import { ARROW, CIRCLE, LINE, RECTANGLE } from "../constants.js";
+
 export class MoveShapeCommand {
   static undoStack = [];
   static redoStack = [];
@@ -11,54 +13,128 @@ export class MoveShapeCommand {
     const top =
       N > 0 ? MoveShapeCommand.undoStack[N - 1] : { id: "", x: "", y: "" };
 
-    if (
-      top.id === this.shape.id &&
-      top.x === this.shape.x &&
-      top.y === this.shape.y
-    ) {
-      return;
+    const shapeType = this.shape.type;
+    if (shapeType === RECTANGLE || shapeType === CIRCLE) {
+      if (
+        top.id === this.shape.id &&
+        top.x === this.shape.x &&
+        top.y === this.shape.y
+      ) {
+        return;
+      }
     }
 
-    MoveShapeCommand.undoStack.push({
-      id: this.shape.id,
-      x: this.shape.x,
-      y: this.shape.y,
-    });
+    if (shapeType === LINE || shapeType === ARROW) {
+      if (
+        top.id === this.shape.id &&
+        top.x1 === this.shape.x1 &&
+        top.y1 === this.shape.y1 &&
+        top.x2 === this.shape.x2 &&
+        top.y2 === this.shape.y2
+      ) {
+        return;
+      }
+    }
 
+    console.log("shapeType", shapeType);
+    if (shapeType === RECTANGLE || shapeType === CIRCLE) {
+      MoveShapeCommand.undoStack.push({
+        id: this.shape.id,
+        x: this.shape.x,
+        y: this.shape.y,
+      });
+    }
+
+    if (shapeType === LINE || shapeType === ARROW) {
+      MoveShapeCommand.undoStack.push({
+        id: this.shape.id,
+        x1: this.shape.x1,
+        y1: this.shape.y1,
+        x2: this.shape.x2,
+        y2: this.shape.y2,
+      });
+    }
+
+    console.log(MoveShapeCommand.undoStack);
     MoveShapeCommand.redoStack = [];
   }
 
   undo() {
     if (MoveShapeCommand.undoStack.length === 0) return;
-    MoveShapeCommand.redoStack.push({
-      id: this.shape.id,
-      x: this.shape.x,
-      y: this.shape.y,
-    });
+    const shapeType = this.shape.type;
+    if (shapeType === RECTANGLE || shapeType === CIRCLE) {
+      MoveShapeCommand.redoStack.push({
+        id: this.shape.id,
+        x: this.shape.x,
+        y: this.shape.y,
+      });
 
-    const lastMovedShape = MoveShapeCommand.undoStack.pop();
-    this.store.shapes.forEach((shape) => {
-      if (shape.id === lastMovedShape.id) {
-        shape.x = lastMovedShape.x;
-        shape.y = lastMovedShape.y;
-      }
-    });
+      const lastMovedShape = MoveShapeCommand.undoStack.pop();
+      this.store.shapes.forEach((shape) => {
+        if (shape.id === lastMovedShape.id) {
+          shape.x = lastMovedShape.x;
+          shape.y = lastMovedShape.y;
+        }
+      });
+    }
+
+    if (shapeType === LINE || shapeType === ARROW) {
+      MoveShapeCommand.redoStack.push({
+        id: this.shape.id,
+        x1: this.shape.x1,
+        y1: this.shape.y1,
+        x2: this.shape.x2,
+        y2: this.shape.y2,
+      });
+
+      const lastMovedShape = MoveShapeCommand.undoStack.pop();
+      this.store.shapes.forEach((shape) => {
+        if (shape.id === lastMovedShape.id) {
+          shape.x1 = lastMovedShape.x1;
+          shape.y1 = lastMovedShape.y1;
+          shape.x2 = lastMovedShape.x2;
+          shape.y2 = lastMovedShape.y2;
+        }
+      });
+    }
   }
 
   redo() {
     if (MoveShapeCommand.redoStack.length === 0) return;
-    MoveShapeCommand.undoStack.push({
-      id: this.shape.id,
-      x: this.shape.x,
-      y: this.shape.y,
-    });
+    const shapeType = this.shape.type;
+    if (shapeType === RECTANGLE || shapeType === CIRCLE) {
+      MoveShapeCommand.undoStack.push({
+        id: this.shape.id,
+        x: this.shape.x,
+        y: this.shape.y,
+      });
 
-    const lastMovedShape = MoveShapeCommand.redoStack.pop();
-    this.store.shapes.forEach((shape) => {
-      if (shape.id === lastMovedShape.id) {
-        shape.x = lastMovedShape.x;
-        shape.y = lastMovedShape.y;
-      }
-    });
+      const lastMovedShape = MoveShapeCommand.redoStack.pop();
+      this.store.shapes.forEach((shape) => {
+        if (shape.id === lastMovedShape.id) {
+          shape.x = lastMovedShape.x;
+          shape.y = lastMovedShape.y;
+        }
+      });
+    }
+    if (shapeType === LINE || shapeType === ARROW) {
+      MoveShapeCommand.undoStack.push({
+        id: this.shape.id,
+        x1: this.shape.x1,
+        y1: this.shape.y1,
+        x2: this.shape.x2,
+        y2: this.shape.y2,
+      });
+
+      const lastMovedShape = MoveShapeCommand.redoStack.pop();
+      this.store.shapes.forEach((shape) => {
+        if (shape.id === lastMovedShape.id) {
+          shape.x1 = lastMovedShape.x1;
+          shape.y1 = lastMovedShape.y1;
+          shape.x2 = lastMovedShape.x2;
+          shape.y2 = lastMovedShape.y2;
+        }
+      });
+    }
   }
 }
